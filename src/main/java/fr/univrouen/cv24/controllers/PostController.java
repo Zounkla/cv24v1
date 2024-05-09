50d6753d5192ae716fb58b6d85c743c33af3011e
vanliflo <florian.van-liedekerke@univ-rouen.fr>
2024-05-09 10:12:17 +0200

    insertion of CV

--- src/main/java/fr/univrouen/cv24/controllers/PostController.java
+++ src/main/java/fr/univrouen/cv24/controllers/PostController.java
@@ -4,14 +4,26 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
 import com.mongodb.client.result.InsertOneResult;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 import org.json.JSONObject;
 import org.json.XML;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.xml.sax.*;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.SchemaFactory;
+import java.io.IOException;
+import java.io.StringReader;
 
 import static com.mongodb.client.model.Filters.eq;
 
@@ -26,7 +38,9 @@ public class PostController {
 
     @RequestMapping(value="/insert", method = RequestMethod.POST, consumes = "application/xml")
     public String insert(@RequestBody String cv) {
-
+        if (!isValidXML(cv)) {
+            return errorInsert("INVALID");
+        }
         JSONObject xmlJsonObject = XML.toJSONObject(cv);
         String jsonString = xmlJsonObject.toString();
         Document doc = Document.parse(jsonString);
@@ -36,7 +50,7 @@ public class PostController {
         MongoDatabase database = mongo.getDatabase("main");
         MongoCollection<Document> collection = database.getCollection("CV24");
         if (identityAlreadyInDB(cv, collection)) {
-            return "a";
+            return errorInsert("DUPLICATED");
         }
         int id = 0;
         Document oldDoc = collection.find(eq("id", id)).first();
@@ -45,8 +59,8 @@ public class PostController {
             oldDoc = collection.find(eq("id", id)).first();
         }
         doc.append("id", id);
-        InsertOneResult result = collection.insertOne(doc);
-        return result.toString();
+        collection.insertOne(doc);
+        return successInsert(id);
     }
 
 
@@ -59,12 +73,16 @@ public class PostController {
      */
     private boolean identityAlreadyInDB(String cv, MongoCollection<Document> collection) {
         String cvName = getInfoFromCV(cv, "<cv24:nom>");
-        Document collectionName = collection.find(eq("cv24:cv24.cv24:identite.cv24:nom",
-                cvName)).first();
-        if (collectionName != null) {
-            return true;
-        }
-        return false;
+        String cvFirstName = getInfoFromCV(cv, "<cv24:prenom>");
+        String cvGender = getInfoFromCV(cv, "<cv24:genre>");
+        String cvTel = getInfoFromCV(cv, "<cv24:tel>");
+        Bson nameFilter = Filters.eq("cv24:cv24.cv24:identite.cv24:nom", cvName);
+        Bson firstNameFilter = Filters.eq("cv24:cv24.cv24:identite.cv24:prenom", cvFirstName);
+        Bson genderFilter = Filters.eq("cv24:cv24.cv24:identite.cv24:genre", cvGender);
+        Bson telFilter = Filters.eq("cv24:cv24.cv24:identite.cv24:tel", cvTel);
+        Document collectionCV = collection.find(Filters.and(nameFilter, firstNameFilter,
+                genderFilter, telFilter)).first();
+        return collectionCV != null;
     }
 
     /**
@@ -79,5 +97,104 @@ public class PostController {
         String closingTag = new StringBuilder(info).insert(1, "/").toString();
         int endIndex = cv.indexOf(closingTag);
         return cv.substring(beginIndex + length, endIndex);
+    }
+
+    /**
+     * errorInsert : returns an XML formatted String showing the error while inserting the CV
+     * @param reason the reason the error occurred
+     * @return String
+     */
+    private String errorInsert(String reason) {
+        return """
+                <error>
+                    <status>ERROR</status>
+                    <detail>%s</detail>
+                </error>
+                """.formatted(reason);
+    }
+
+    /**
+     * successInsert : returns an XML formatted String showing the success while inserting the CV
+     * @param id the id of the CV inserted
+     * @return String
+     */
+    private String successInsert(int id) {
+        return """
+                <success>
+                    <id>%d</id>
+                    <status>INSERTED</status>
+                </success>
+                """.formatted(id);
+    }
+
+    /**
+     * checks if a CV is valid against CV XSD
+     * @param CV the parsed CV
+     * @return boolean
+     */
+    private boolean isValidXML(String CV) {
+        SAXParserFactory spf = SAXParserFactory.newInstance();
+        spf.setNamespaceAware(true);
+        spf.setValidating(false);
+        SchemaFactory sf = SchemaFactory.newInstance("http://www.w3.org/2001/XMLSchema");
+        try {
+            spf.setSchema(sf.newSchema(
+                    new Source[] {new StreamSource("src/main/resources/xml/cv24.tp1.xsd")}
+            ));
+        } catch (SAXException e) {
+            throw new RuntimeException(e);
+        }
+        SAXParser parser;
+        try {
+            parser = spf.newSAXParser();
+        } catch (ParserConfigurationException | SAXException e) {
+            throw new RuntimeException(e);
+        }
+        XMLReader reader;
+        try {
+            reader = parser.getXMLReader();
+        } catch (SAXException e) {
+            throw new RuntimeException(e);
+        }
+        SimpleErrorHandler seh = new SimpleErrorHandler();
+        reader.setErrorHandler(seh);
+        try {
+            try {
+                reader.parse(new InputSource(new StringReader(CV)));
+            } catch (SAXException e) {
+                throw new RuntimeException(e);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return !seh.hasError();
+    }
+
+    private static class SimpleErrorHandler implements ErrorHandler {
+
+        private boolean errorOccured;
+
+        public SimpleErrorHandler() {
+
+        }
+
+        public boolean hasError() {
+            return errorOccured;
+        }
+
+        @Override
+        public void warning(SAXParseException exception) throws SAXException {
+            errorOccured = true;
+        }
+
+        @Override
+        public void error(SAXParseException exception) throws SAXException {
+            errorOccured = true;
+        }
+
+        @Override
+        public void fatalError(SAXParseException exception) throws SAXException {
+            errorOccured = true;
+        }
     }
 }
